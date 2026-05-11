1f4209e79e1cff3596ad3bf730665ab7a510c597
Anton Litvinov <litvinov@jetshr.com.br>
2026-05-11 15:43:07 -0600

    Fix horarios month horizontal scroll

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE = "jet-v26";
+const CACHE = "jet-v27";
 
 // Сообщение от клиента «активируй новую версию сразу» (без ожидания закрытия вкладки)
 self.addEventListener("message", e => {
